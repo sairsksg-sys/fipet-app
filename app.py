a6c1d911a436d8cbf963529c9fdf75c7179984e7
sairsk <sairsk.sg@gmail.com>
2025-12-29 12:56:26 +0800

    Updated port info

--- app.py
+++ app.py
@@ -228,4 +228,8 @@ scheduler.add_job(send_reminders, "interval", hours=24)
 scheduler.start()
 
 if __name__=="__main__":
-    app.run()
+#    app.run()
+     app.run(
+          host="0.0.0.0",
+          port=int(os.environ.get("PORT", 8080))
+     )
